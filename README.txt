3dde9c61118508b42ce647053987e5b494db2463
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-06-14 09:36:16 +0300

    Initial code import.

---
+++ README.txt
@@ -0,0 +1,8 @@
+Blank Project
+=============
+
+An initial project template with some database setup and buidout
+goodies.
+
+
+
---
+++ src/blank/views.py
@@ -0,0 +1,7 @@
+from blank.models import DBSession
+from blank.models import MyModel
+
+def my_view(request):
+    dbsession = DBSession()
+    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+    return {'root':root, 'project':'blank'}
